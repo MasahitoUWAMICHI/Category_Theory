ded3652782465a4f4167f1bc29bad614af129b73
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2025-01-04 03:26:30 +0900

    l

--- check_base_quiver.hs
+++ check_base_quiver.hs
@@ -1,4 +1,4 @@
-:l src/base.hs -- base.hsをロード
+l: src/base.hs -- base.hsをロード
 
 instance Quiver BoolQuiver where
     data QuiverData BoolQuiver = QuiverData { vertices = [True, False], arrows = [TrueToFalse, FalseToTrue], source = \case TrueToFalse -> True; FalseToTrue -> False, target = \case TrueToFalse -> False; FalseToTrue -> True }
