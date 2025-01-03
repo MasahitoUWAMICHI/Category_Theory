8cfa9fec95e19bace4e269b6ee360bfea42da4b1
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2025-01-04 03:43:15 +0900

    comment out

--- src/base.hs
+++ src/base.hs
@@ -3,8 +3,8 @@ module CategoryBase where
 data QuiverData a b = QuiverData { vertices :: a, arrows :: b, source :: b -> a, target :: b -> a }
 class Quiver q where
     data QuiverData q
-    vertices :: q -> a
-    arrows :: q -> b
-    source :: b -> a
-    target :: q -> b -> a
+    --vertices :: q -> a
+    --arrows :: q -> b
+    --source :: b -> a
+    --target :: q -> b -> a
 
