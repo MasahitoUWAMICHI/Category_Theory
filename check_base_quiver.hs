3863e67942cecebb2ac6099e17a24d4a5d3e40e2
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2025-01-04 03:25:36 +0900

    print

--- check_base_quiver.hs
+++ check_base_quiver.hs
@@ -3,9 +3,11 @@
 instance Quiver BoolQuiver where
     data QuiverData BoolQuiver = QuiverData { vertices = [True, False], arrows = [TrueToFalse, FalseToTrue], source = \case TrueToFalse -> True; FalseToTrue -> False, target = \case TrueToFalse -> False; FalseToTrue -> True }
 
-vertices BoolQuiver -- [True, False]
-arrows BoolQuiver -- [TrueToFalse, FalseToTrue]
-source TrueToFalse -- True
-target TrueToFalse -- False
-source FalseToTrue -- False
-target FalseToTrue -- True
+main = do
+    print $ vertices BoolQuiver
+    print $ arrows BoolQuiver
+    print $ source BoolQuiver TrueToFalse
+    print $ target BoolQuiver TrueToFalse
+    print $ source BoolQuiver FalseToTrue
+    print $ target BoolQuiver FalseToTrue
+
